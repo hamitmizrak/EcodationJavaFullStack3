b993db7119e718df9236bfce6053b3031d5ae432
hamitmizrak <hamitmizrak@gmail.com>
2022-07-30 11:46:37 +0300

    PSVM

---
+++ src/main/java/com/hamitmizrak/java/_003_Psvm.java
@@ -0,0 +1,12 @@
+package com.hamitmizrak.java;
+
+public class _003_Psvm {
+
+    //psvm ===> TAB
+    public static void main(String[] args) {
+
+        //sout ==> TAB
+        System.out.println("Merhaba Dünya");
+    }
+
+}
