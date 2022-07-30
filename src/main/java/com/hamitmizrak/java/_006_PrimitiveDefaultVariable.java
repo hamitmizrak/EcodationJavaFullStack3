158814b3ecf8cf40a040d17a604c74061b852a25
hamitmizrak <hamitmizrak@gmail.com>
2022-07-30 12:10:03 +0300

    primitive default veri

---
+++ src/main/java/com/hamitmizrak/java/_006_PrimitiveDefaultVariable.java
@@ -0,0 +1,38 @@
+package com.hamitmizrak.java;
+
+public class _006_PrimitiveDefaultVariable {
+
+    //psvm ===> TAB
+    public static void main(String[] args) {
+
+
+
+        //Primitive Type
+        // 8 tane türü vardır.
+        //ilke Türler javanın 100/100 OOP olmamasına neden olandır.
+        //Hafıza Stack Memory kullanır.
+        //instance (new ) kullanmadan çalışır.
+        //null değer alamazlar.
+        //bazen primitive türleri Wrapper yapılara çevirmekn zorunda kalırız.
+
+
+        // primitive Type : 8 tane primitive Type vardır.
+        // tamsayılarda default : 0 (Sıfır):  b<s<i<l
+        byte b1;
+        short s1;
+        int i1;
+        long l1L;
+
+        // virgüllü sayılarda default sayı 0.0(sıfır): float<double
+        float f2;
+        double d2;
+
+        // boolean default veri false:
+        boolean b3;
+
+        //karakter default veri '\u0000'': char
+        char c4;
+
+    }
+
+}
