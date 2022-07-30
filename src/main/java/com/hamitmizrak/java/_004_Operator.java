dc27798bea9ad4187bd0afed6b411792df8e1963
hamitmizrak <hamitmizrak@gmail.com>
2022-07-30 11:46:54 +0300

    Operators

---
+++ src/main/java/com/hamitmizrak/java/_004_Operator.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.java;
+
+public class _004_Operator {
+
+    //psvm ===> TAB
+    public static void main(String[] args) {
+         //Aritmetik operatörler: + -  * / %
+        //ilişkisel operatörler < <= > >=
+        //Mantıksal Operatörler:   &&=VE    ||=VEYA      !=DEĞİL
+        //Atama operatörler:  String kelime="Malatya"    +=,   -= ,*=, /=
+    }
+
+}
