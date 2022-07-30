9f3d241ba75fee57a6a8080fdb6095c76e45c230
hamitmizrak <hamitmizrak@gmail.com>
2022-07-30 11:21:40 +0300

    Variable

---
+++ src/main/java/com/hamitmizrak/java/_002_Variable.java
@@ -0,0 +1,34 @@
+package com.hamitmizrak.java;
+
+public class _002_Variable {
+
+
+    /*
+    Variable: Değişken
+    camelCase yazalım Örneğin:  String degiskenAdi;
+    Değişken adları isim,sıfat olacak şekilde kurgulabiliriz.
+    Kısa ve öz olmalı gelecekteki biz onu okuduğumuzda hemen anlamalıyız.
+    değişkenlerleri ingilizce kelimeler kullanırsak çok daha iyidir
+    Türkçedeki üğşçö kullanmayalım kullanırsak birşey olmaz ancak compilerdan sonra bu harflerin karşılığı yoksa hata verir
+    String çözümDüğümü;
+    Unicode Ascii code nedir ?
+     */
+
+
+    //camelCase
+    String degiskenAdi;
+
+    //Dolar veya altçizgi(_) ile başlayabiliriz. bunların hariçinde sayı veya özel singelerle başlanmaz
+    String $degiskenAdi;
+    String _degiskenAdi;
+
+    //Dolar veya altçizgi(_) ile başlayabiliriz. bunların hariçinde sayı veya özel singelerle başlanmaz
+    String +_degiskenAdi;
+    String ~_degiskenAdi;
+    String &_degiskenAdi;
+
+    //Değişkenler sayı ile başlamaz ancak sayı ile bitebilir.
+    String 44_degiskenAdi;
+    String _degiskenAdi44;
+
+}
