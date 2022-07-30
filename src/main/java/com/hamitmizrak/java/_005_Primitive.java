dc958d0276a631f09bc1bad0fc14e8ac766a944f
hamitmizrak <hamitmizrak@gmail.com>
2022-07-30 12:00:53 +0300

    primitive

---
+++ src/main/java/com/hamitmizrak/java/_005_Primitive.java
@@ -0,0 +1,47 @@
+package com.hamitmizrak.java;
+
+public class _005_Primitive {
+
+    //psvm ===> TAB
+    public static void main(String[] args) {
+
+        //Syntax Error
+        //logical Error
+        //Run time Error
+        //Compiler Time Error
+
+        //cast: f-L
+
+        //Primitive Type
+        // 8 tane türü vardır.
+        //ilke Türler javanın 100/100 OOP olmamasına neden olandır.
+        //Hafıza Stack Memory kullanır.
+        //instance (new ) kullanmadan çalışır.
+        //null değer alamazlar.
+        //bazen primitive türleri Wrapper yapılara çevirmekn zorunda kalırız.
+
+
+        // primitive Type : 8 tane primitive Type vardır.
+        // tamsayılar:  b<s<i<l
+        byte b1 = 127;
+        short s1 = 128;
+        int i1 = 1555155;
+        long l1 = 151515151545L;
+
+        // virgüllü sayılar: float<double
+        float f2=15.45f;
+        double d2=1555115.415;
+
+        // boolean: true veya false
+        boolean b3=true;
+        b3=false;
+        b3=3>1;//true
+        b3=1>5; //false
+
+        //karakter: char
+        char c4='@';
+        System.out.println(c4);
+
+    }
+
+}
