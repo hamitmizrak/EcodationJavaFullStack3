36ba0ff15e872a23e3294bf512c76cae3e41cbc7
hamitmizrak <hamitmizrak@gmail.com>
2022-07-31 10:02:23 +0300

    System

---
+++ src/main/java/com/hamitmizrak/java/_013_System.java
@@ -0,0 +1,27 @@
+package com.hamitmizrak.java;
+
+import java.util.Date;
+
+public class _013_System {
+
+    //psvm ===> TAB
+    public static void main(String[] args) {
+
+        //System
+        //System.in    ==>  Javaya Veri girişi
+        //System.out  ==> Javadan Veri çıkışı
+        System.out.print("Bundan sonraki alt satıra geçirmez");
+        System.out.println("Deneme");
+        System.err.println("Error C");
+
+        //Şu andaki zamanı verir
+        System.out.println(new Date(System.currentTimeMillis()));
+
+        //Garbarage Collection
+        System.gc();
+
+        //logout
+        System.exit(0);
+
+    }
+}
