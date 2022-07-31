4391659c634543d65218a9c349c1878ca6779857
hamitmizrak <hamitmizrak@gmail.com>
2022-07-31 10:37:13 +0300

    Math

---
+++ src/main/java/com/hamitmizrak/java/_016_Math.java
@@ -0,0 +1,38 @@
+package com.hamitmizrak.java;
+
+public class _016_Math {
+
+    public static void main(String[] args) {
+        //Math: Mathematics kısaltmasıdır
+        //Math sınıf static bir yapıya sahiptir
+        //java.lang kütüphanesinden import ederiz.
+        //Math iç içe yazabilirsiniz ==>  Math.round(Math.random())
+
+        // 2 tane sabitimiz vardır
+        //Math.PI  ==> 3.14 (final double)
+        //Math.E   ==> 2.71 (final double)
+
+        //Sabitler
+        System.out.println("PI SAYISI: "+Math.PI);
+        System.out.println("E SAYISI: "+Math.E);
+
+        //Hesap işlemler
+        System.out.println("KAREKÖK: "+Math.sqrt(16));
+        System.out.println("MUTLAK DEĞER: "+Math.abs(-4));
+        System.out.println("ÜSLÜ DEĞER: "+Math.pow(2,5));
+
+        //min max
+        System.out.println("MIN: "+Math.min(-2,500));
+        System.out.println("MAX: "+Math.max(-2,500));
+
+        //Yuvarlama
+        System.out.println("YUKARI YUVARLA: "+Math.ceil(4.1));
+        System.out.println("AŞAĞI YUVARLA: "+Math.floor(4.1));
+        System.out.println("AŞAĞI/YUKARI YUVARLA: "+Math.round(6.5)); //.5 ve yukarı ise yukarı yuvarlar
+        System.out.println("AŞAĞI/YUKARI YUVARLA: "+Math.round(6.4)); //.4 ve yukarı ise aşağı yuvarlar
+
+        //Rastgele
+        System.out.println(Math.round(Math.random()*4+1));
+
+    }
+}
