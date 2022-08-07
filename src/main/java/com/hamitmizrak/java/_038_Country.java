bb3e97b86c11e59a2921301e271daaaab3f76aa9
hamitmizrak <hamitmizrak@gmail.com>
2022-08-07 09:27:41 +0300

    Inner Class

---
+++ src/main/java/com/hamitmizrak/java/_038_Country.java
@@ -0,0 +1,33 @@
+package com.hamitmizrak.java;
+
+// 1 tane public class yazabilirsiniz (Inner Class Hariç)
+// classlarda static olamaz (Inner Class Hariç)
+
+import lombok.Getter;
+import lombok.Setter;
+
+public  class _038_Country {
+
+    @Getter @Setter
+    private String countryName;
+
+    //inner class
+   public  static  class City {
+
+        @Getter @Setter
+        private String cityName;
+    }
+}
+
+//psvm
+class MainClass{
+    public static void main(String[] args) {
+        _038_Country country=new _038_Country();
+        country.setCountryName("Türkiye");
+
+       _038_Country.City city=new _038_Country.City();
+       city.setCityName("Ankara");
+
+        System.out.println("Ülke Adı: "+country.getCountryName()+" Şehir Adı: "+city.getCityName());
+    }
+}
